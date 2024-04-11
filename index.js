900281401c5d4c9d428bca07fefcfed5b1513d2d
Heshi Macharia <heshi.macharia@student.moringaschool.com>
2024-04-11 15:41:01 +0300

    second-commit

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 const baseUrl = 'http://localhost:5500'
 fetch(`${baseUrl}/results`)
 .then(function (response){
-    return response.json
+    return response.json;
 })
- 
-function checkOption(chosenOption) {
-    if(chosenOption = wrongOption){
-        console.log(`${chosenOption} is correct!`);
+.then(function (data) {
+    console.log(data);
+})
+
+function checkAnswer(inputAnswer) {
+    if(inputAnswer = incorrectAnswer){
+        document.getElementById("correct_answer");
+        return `${inputAnswer} is correct!`;
     }
-    else if(chosenOption = wrongOption){
-        console.log(`${chosenOption} is not correct.`);
+    else if(inputAnswer = wrongOption){
+        document.getElementById("incorrect_answers")
+        return `${inputAnswer} is not correct.`;
     }
     else{
         alert(`Please choose an option before proceeding to the next question`);
     }
 }
 
-function calculate(totalScore) {
-    if(totalScore = 100){
-        return "Congratulations! You aced the trivia!";
-    }
-    else if(totalScore < 100 || totalScore >= 50){
-        return "Not bad";
-    }
-}
+
